8a086d22ae93fd25521d7b6e3d9e39edda17a2f9
Vicky Poonia <vickychaudhary4596@gmail.com>
2024-11-18 14:30:00 +0530

    blank

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-
-const App = () => {
-	return <div>My App!</div>;
-};
-
-export default App;
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const appElement = document.getElementById("app");
-
-const root = createRoot(appElement!);
-root.render(<App />);
